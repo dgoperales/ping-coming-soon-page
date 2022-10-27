323f381ea2bf1311feecb88068288b1461a694d7
Diego Perales <dperales1313@gmail.com>
2022-10-27 14:04:28 -0400

    Al ingresar un correo invalido aparece el mensaje de error en la versión mobile.

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ function validate() {
     const mail = document.getElementById("email").value;
     const validation = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
     const errorInput = document.getElementById("email");
+    const errorMessage = document.getElementById("error-message");
 
     if(validation.test(mail)) {
-        alert("Correct");
+        errorInput.style.borderColor = 'hsl(223, 100%, 88%)'
+        errorMessage.style.display = 'none';
         return true;
     } else {
         errorInput.style.borderColor = 'hsl(354, 100%, 66%)';
+        errorMessage.style.display = 'block';
+        errorMessage.style.position = 'relative'; 
+        errorMessage.style.padding = '0 0 2rem 0';
         return false
     }
 }
